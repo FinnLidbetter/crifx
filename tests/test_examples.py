f4b663eb8fb00e9d48ec2b689dda13627297d008
Finn Lidbetter <finnlidbetter@gmail.com>
2024-06-09 16:19:24 -0400

    Rename test_output to .test_output
    This is an attempt to make it clear that there is not a problem
    called test_output in the problemset.

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -6,7 +6,7 @@ import os
 def test_example_problemset(examples_path):
     """Test that the example problemset is being parsed as expected."""
     example_problemset_path = os.path.join(examples_path, "example_problemset")
-    example_test_output = os.path.join(example_problemset_path, "test_output")
+    example_test_output = os.path.join(example_problemset_path, ".test_output")
     status = os.system(
         f"crifx {example_problemset_path} --output-dir {example_test_output}"
     )
